b7001074bf51a7c9fbe07f6b3692ce79dc153db9
piperching <jan_kristoffer96@yahoo.co.uk>
2016-02-27 21:40:24 +0800

    Added dependencies
    removed query returnable

--- src/view/Observer.java
+++ src/view/Observer.java
@@ -2,8 +2,6 @@ package view;
 
 import java.util.ArrayList;
 
-import model.QueryReturnable;
-
 public interface Observer {
 	public void update(String[] columnNames, Object[][] data);
 }
--- src/view/Table.java
+++ src/view/Table.java
@@ -40,8 +40,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-import model.QueryReturnable;
-
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
